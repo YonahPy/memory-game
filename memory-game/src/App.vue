d0a3be2015f42aa6287673ee8e843a2533b735fb
YonahPy <jrjonas487@gmail.com>
2023-12-21 17:24:51 -0300

    first commit

---
+++ memory-game/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>ola mundo</div>
+  <card></card>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import card from './components/card.vue';
+</script>
+
+<style scoped>
+
+</style>
